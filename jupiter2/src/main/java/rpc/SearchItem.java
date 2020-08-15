9b610bf825da73e68af81061c21e1817997f4af1
OliLobster <sgyr1993@163.com>
2020-08-14 23:05:40 -0700

    Update SearchItem.java
    update doGet() to use GitHubClient

--- jupiter2/src/main/java/rpc/SearchItem.java
+++ jupiter2/src/main/java/rpc/SearchItem.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
+import external.GitHubClient;
+
 /**
  * Servlet implementation class SearchItem
  */
@@ -32,11 +34,12 @@ public class SearchItem extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		JSONArray array = new JSONArray();
-		array.put(new JSONObject().put("username", "abcd"));
-		array.put(new JSONObject().put("username", "1234"));
-		RpcHelper.writeJsonArray(response, array);
-		
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+
+		GitHubClient client = new GitHubClient();
+		RpcHelper.writeJsonArray(response, client.search(lat, lon, null));
+
 	}
 
 	/**
