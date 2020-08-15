08156c3f6ee72bcec0ebc51a11bfbb041458e3d1
OliLobster <sgyr1993@163.com>
2020-08-14 22:37:44 -0700

    Add constants to GitHubClient Class

--- jupiter2/src/main/java/external/GitHubClient.java
+++ jupiter2/src/main/java/external/GitHubClient.java
@@ -1,5 +1,7 @@
 package external;
 
 public class GitHubClient {
-
+	private static final String URL_TEMPLATE = "https://jobs.github.com/positions.json?description=%s&lat=%s&long=%s";
+	private static final String DEFAULT_KEYWORD = "developer";
 }
+
