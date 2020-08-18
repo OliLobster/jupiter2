b40a3474a308706021eb57edc95cb026b480d4d9
OliLobster <sgyr1993@163.com>
2020-08-17 21:16:33 -0700

    Update Item.java

--- jupiter2/src/main/java/entity/Item.java
+++ jupiter2/src/main/java/entity/Item.java
@@ -9,24 +9,63 @@ public class Item {
 	private Set<String> keywords;
 	private String imageUrl;
 	private String url;
-	
+
 	public String getItemId() {
 		return itemId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public Set<String> getKeywords() {
 		return keywords;
 	}
+
 	public String getImageUrl() {
 		return imageUrl;
 	}
+
 	public String getUrl() {
 		return url;
 	}
-	
+
+	public static class ItemBuilder {
+		private String itemId;
+		private String name;
+		private String address;
+		private String imageUrl;
+		private String url;
+		private Set<String> keywords;
+
+		public void setItemId(String itemId) {
+			this.itemId = itemId;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		public void setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		public void setKeywords(Set<String> keywords) {
+			this.keywords = keywords;
+		}
+
+	}
+
 }
